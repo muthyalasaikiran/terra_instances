c4fac8422546d1019d8b32fb5e1c5b4c5d95c94e
saikiran <muthyalasaikiran1999@gmail.com>
2024-01-13 08:17:50 +0530

    adding subnets web and app

--- inputs.tf
+++ inputs.tf
@@ -1,6 +1,15 @@
-
+# vpc variable
 variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
   description = "this is cidr range for vpc" # for dynamic nature creating variables this inputs file
 }
+
+# subnet variable
+
+variable "subnames" {
+  type    = list(string)
+  default = ["web1", "app2"]
+
+}
+
--- network.tf
+++ network.tf
@@ -1,3 +1,5 @@
+#  vpc
+
 resource "aws_vpc" "terra_vpc" {
   cidr_block =  var.vpc_cidr
 
@@ -7,3 +9,14 @@ resource "aws_vpc" "terra_vpc" {
   }
 }
 
+# subnet 
+
+resource "aws_subnet" "subnets" {
+  count      = length(var.subnames)
+  vpc_id     = aws_vpc.terra_vpc.id
+  availability_zone = "us-east-1c"
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index) #cidr_block = format(var.subnet_cidr_format,count.index)
+  tags = {
+    Name = var.subnames[count.index]
+  }
+}
